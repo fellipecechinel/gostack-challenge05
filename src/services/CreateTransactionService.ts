d093cad71420668d4b0f9794a099ab9abbacc8f7
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-04-19 00:20:48 -0300

    Added CreateTransactionService method

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Resquest {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Resquest): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is not valid');
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      const newTotal = balance.total - value;
+      if (newTotal < 0) {
+        throw Error(
+          'Cannot create outcome transaction without a valid balance',
+        );
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
